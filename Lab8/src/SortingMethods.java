e69f930c118f97597dce20372a25e5b0d6d2ffd9
Ben Simondet <simon998@morris.umn.edu>
2014-10-28 13:15:58 -0500

    Finished with lab 8

--- Lab8/src/SortingMethods.java
+++ Lab8/src/SortingMethods.java
@@ -1,12 +1,13 @@
+//Aaron Lemmon and Ben Simondet
 public class SortingMethods {
 	/*
 	 * sorts an array using insertion sort
 	 */
 	public static void insertionSort(Comparable[] arr) {
-		
+
 		for (int i = 1; i < arr.length; i++) {
 			Comparable nextItem = arr[i];
-			
+
 			int j = i;
 			while (j > 0 && nextItem.compareTo(arr[j - 1]) < 0) {
 				arr[j] = arr[j - 1];
@@ -26,43 +27,57 @@ public class SortingMethods {
 		 * hack, but it works.
 		 */
 		if (arr.length > 1) {
-			Comparable[] leftHalf = new Comparable[arr.length / 2];
-			for (int i = 0; i < arr.length / 2; i++) {
+
+			int leftArrayLength = arr.length / 2;
+			Comparable[] leftHalf = new Comparable[leftArrayLength];
+			Comparable[] rightHalf = new Comparable[arr.length - leftHalf.length];
+
+			// Fill leftHalf
+			for (int i = 0; i < leftArrayLength; i++) {
 				leftHalf[i] = arr[i];
 			}
-			Comparable[] rightHalf = new Comparable[arr.length - leftHalf.length];
-			for (int i = arr.length/2; i < arr.length; i++) {
-				rightHalf[i - arr.length/2] = arr[i];
+
+			// Fill rightHalf
+			for (int i = leftArrayLength; i < arr.length; i++) {
+				rightHalf[i - leftArrayLength] = arr[i];
 			}
+
 			mergeSort(leftHalf);
 			mergeSort(rightHalf);
 			merge(arr, leftHalf, rightHalf);
 		}
-		
 	}
 
-
+	/*
+	 * Merges two sorted arrays (leftHalf and rightHalf) together into a larger sorted array.
+	 */
 	private static void merge(Comparable[] arr, Comparable[] leftHalf, Comparable[] rightHalf) {
+		
 		int nextIndex = 0;
 		int leftIndex = 0;
 		int rightIndex = 0;
-		while(leftIndex < leftHalf.length && rightIndex < rightHalf.length){
+		/*
+		 * Rebuilds the array until there is nothing left to sort
+		 */
+		while (leftIndex < leftHalf.length && rightIndex < rightHalf.length) {
 			if (leftHalf[leftIndex].compareTo(rightHalf[rightIndex]) <= 0) {
 				arr[nextIndex++] = leftHalf[leftIndex++];
-				
 			} else {
 				arr[nextIndex++] = rightHalf[rightIndex++];
 			}
 		}
-		if(leftIndex > leftHalf.length){
-			for(int i = rightIndex; i < rightHalf.length; i++){
+		/*
+		 * Copy over the remaining portion of the array
+		 */
+		if (leftIndex == leftHalf.length) {
+			for (int i = rightIndex; i < rightHalf.length; i++) {
 				arr[nextIndex++] = rightHalf[i]; 
 			}
 		}
-		else{
-			for(int i = leftIndex; i < leftHalf.length; i++){
+		else {
+			for (int i = leftIndex; i < leftHalf.length; i++) {
 				arr[nextIndex++] = leftHalf[i];
 			}
 		}
-	}
+	} 
 }
